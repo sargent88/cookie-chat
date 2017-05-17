f3ba30d56c1d9a5b976e26ffb0f23dbc958ee908
sargent88 <steen.sargent@gmail.com>
2017-05-17 15:11:03 -0600

    cookie chat

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,32 @@
-angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+angular.module('chatroom').controller('mainCtrl', function($scope, messageService, $http) {
 
-  //The getMessages function will call the getMessages method on the messageService object.
-  //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+    //The getMessages function will call the getMessages method on the messageService object.
+    //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+    $scope.getMessages = function() {
+        var promiseGet = messageService.getMessages();
+        promiseGet.then(function(response) {
+            $scope.messages = response.data.reverse();
+        })
+    }
+    $scope.getMessages();
 
 
 
+    //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+    //pass that text to the postMessage method on the messageService object which will
+    //then post it to the backend.
+    $scope.postMessage = function() {
+        messageService.postMessage($scope.message);
+        $scope.message = ''
 
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
-  //pass that text to the postMessage method on the messageService object which will
-  //then post it to the backend.
+    }
 
 
+    //uncomment this code when your getMessages function is finished
+    //This goes and gets new data every second, which mimicking a chat room experience.
+    setInterval(function() {
+        $scope.getMessages();
 
-  //uncomment this code when your getMessages function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
-
-  // }, 1500)
+    }, 1500)
 
 })
